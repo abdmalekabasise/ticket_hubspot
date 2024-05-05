5ece350196993fa131f40c71d4e17db5b6bedc64
Malek Abasise <abdmalek.abassisse@esprit.tn>
2024-05-05 02:01:26 +0100

    outtt

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
 
     domains: ['seatgeek.com', 'images.gametime.co', 'd2o50i5c2dr30a.cloudfront.net', 'img.vggcdn.net'], // Add your local domain here
   },
-  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
